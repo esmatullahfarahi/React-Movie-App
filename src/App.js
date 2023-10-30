dce9738d230f18ee12828fa2164e59e5336063d0
Esmat Farahi <esmatullah.farahi@gmail.com>
2023-10-30 15:55:44 -0400

    API call and Css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import {useEffect} from "react";
+import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL='http://www.omdbapi.com/?apikey=d5322ef2';
 
 const App = () =>  {
-    return (<h1>App</h1>);
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data=await response.json();
+         
+        console.log(data.Search);
+    }
+
+
+    useEffect(()=>{
+      searchMovies('Spiderman')
+    }, []);
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+            <div className="search">
+            <input placeholder="Search for Movies" value="Superman" />
+            </div>
+        </div>
+    );
 
 }
 
