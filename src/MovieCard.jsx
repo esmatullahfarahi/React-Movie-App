d965f056193e5259763bff1e31ad52e3d6d908ca
Esmat Farahi <esmatullah.farahi@gmail.com>
2023-10-31 16:07:04 -0400

    search function for movies

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 
-const MovieCard= ({movie})=> {
-    return (
-          <div className="movie">
-                <div>
-                    <p>
-                        {movie.Year}
-                    </p>
-                </div>
-                <div>
-                    <img src={movie.Poster !=='N/A' ? movie.Poster : 'https://via.placeholder.com/400' } alt="movie1" />
-                                    </div>
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie">
+      <div>
+        <p>{movie.Year}</p>
+      </div>
+      <div>
+        <img
+          src={
+            movie.Poster !== "N/A"
+              ? movie.Poster
+              : "https://via.placeholder.com/400"
+          }
+          alt="movie1"
+        />
+      </div>
 
-             <div>
-                 <span>{movie.Type}</span>
-                 <h3>{movie.Title}</h3>
-              </div>
-       </div>
-    )
-}
+      <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+      </div>
+    </div>
+  );
+};
 
-export default MovieCard
+export default MovieCard;
